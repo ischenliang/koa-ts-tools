470a3f9e19dfedb623eb98663d9749fc9172fe37
itchenliang <itchenliang@163.com>
2022-09-20 14:31:09 +0800

    feat:测试

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 const AV = require('leanengine')
 AV.init({
-  appId: 'rzTb02G2QSk310hR5O2CTnig-gzGzoHsz',
-  appKey: 'VmwFVP7TBGvzc4Dhv56LgFD1',
-  masterKey: 'gkHsypYYsgyUqCvTt2LQhPOM',
-  serverURLs: 'https://rztb02g2.lc-cn-n1-shared.com'
+  appId: process.env.LEANCLOUD_APP_ID || 'rzTb02G2QSk310hR5O2CTnig-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'VmwFVP7TBGvzc4Dhv56LgFD1',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'gkHsypYYsgyUqCvTt2LQhPOM'
 });
 
 var app = require('./server');
 
 // 监听端口
-app.listen(80, () => {
+app.listen(process.env.LEANCLOUD_APP_PORT || 80, () => {
   console.log(' DONE ', 'Compiled successfully in 10ms');
   console.log(`访问启动成功：`, 'http://localhost:80');
 })
